6435732aac30a398f92323e7b486cf09d6846a00
kvoid <kianheitkamp@outlook.com>
2024-01-21 23:17:58 +0100

    experimental rust changes

--- Legacy-rust-version/src/main.rs
+++ Legacy-rust-version/src/main.rs
@@ -1,6 +1,7 @@
 #![allow(non_snake_case)]
 #![cfg_attr(all(target_os = "windows", not(debug_assertions)), windows_subsystem = "windows")]
 
+/*
 use dvd::Dvd;
 use raylib_ffi::colors;
 use raylib_port::{init_audio_device, Window, begin_drawing, clear_background, end_drawing, Image, Rectangle};
@@ -14,7 +15,7 @@ mod  raylib_port;
 mod gol_consts;
 mod dvd;
 
-/*
+
 // game trait
 mod game;
 
@@ -40,40 +41,21 @@ use selector::Selector;
 use std::time::{UNIX_EPOCH, SystemTime};
 */
 
-use game::Game;
+use raylib::{drawing::RaylibDraw, color::Color};
 
 fn main() {
-    // seed rand
-    init_audio_device();
+    let (mut rl, th) = raylib::init()
+        .width(600).height(500).build();
 
-    let mut window = Window::init(500, 500, "Hello, World!");
-    window.set_target_fps(120);
-    window.set_exit_key(raylib_ffi::enums::KeyboardKey::Null);
-    //window.set_icon();
+    while !rl.window_should_close() {
+        let mut dr = rl.begin_drawing(&th);
+        dr.clear_background(Color::BLACK);
 
-    window.show();
-
-    let mut dvd = Dvd::new();
-
-    while !window.should_close() {
-        begin_drawing();
-        dvd.update();
-        end_drawing();
+        for y in 0..50 {
+            for x in 0..60 {
+                dr.draw_text("#", x, y, 100, Color::GRAY);
+            }
+        }
+        dr.draw_fps(0, 0);
     }
-
-    /*
-
-    // instantiate games
-    let mut selector = Selector::new();
-
-    // FUCK the person who made this function async!
-    selector.set_pong_sound(load_sound_from_bytes(
-        include_bytes!("../assets/beep.wav")
-    ).await.unwrap());
-
-    loop {
-        selector.update();
-        next_frame().await;
-    }
-    */
 }
